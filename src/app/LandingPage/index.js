76c6dc801cba59a9b8d73a8462a54c095c7d83be
Holly Lindsey <lindsey.holly@yahoo.com>
2022-02-14 17:52:15 -0500

    Semantic UI positioning

--- src/app/LandingPage/index.js
+++ src/app/LandingPage/index.js
@@ -9,130 +9,195 @@ const LandingPage = () => {
     "https://drive.google.com/file/d/0B8_Fz4m5hcoiTXpTbklDOF9iUHc/preview?resourcekey=0-xI_68aH1lllySOdEovKvPQ";
   return (
     <React.Fragment>
-      <h1 class="ui center aligned grid">Dungeon World Manager</h1>
-      <Grid stackable>
-        {/* About Dungeon World */}
-        <h2 class="ui center aligned grid">Dungeon World</h2>
-        {/* Table Top RPG */}
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <Icon name="shield" />
-            {/* <Icon name="compass outline" />
-            <Icon name="fire" />
-            <Icon name="magic" /> */}
-            <h3>What is Dungeon World?</h3>
-            <p>Dungeon World is an open source table top roleplaying game.</p>
-          </Grid.Column>
-          {/* Exp from building the story */}
-          <Grid.Column>
-            <Icon name="conversation" />
-            {/* <Icon name="beer" /> */}
-            <h3>Gain experience from building the story</h3>
-            <p>
-              Dungeon World's simple rules happen based on what's happening
-              within the game, so you spend more time talking about the action
-              and less time talking about the rules.
-            </p>
-          </Grid.Column>
-          {/* Vs DnD */}
-          <Grid.Column>
-            <Icon name="trophy" />
-            <h3>How's it different from DnD?</h3>
-            <p>Dungeon World uses the Powered by the Apocalypse engine.</p>
-            <p>
-              Story-driven world building and character development - experience
-              is gained by progressing the story and developing your character.
-            </p>
-          </Grid.Column>
-        </Grid.Row>
+      <h1 class="ui center aligned header">Dungeon World Manager</h1>
+      {/* About Dungeon World */}
+      <h2 class="ui center aligned header">Dungeon World</h2>
+      {/* Table Top RPG */}
+      <Grid centered columns={3}>
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="shield" size="large" />
+              {/* <Icon name="compass outline" size="large" />
+            <Icon name="fire" size="large" />
+            <Icon name="magic" size="large" /> */}
+            </Grid.Column>
+            <Grid.Column>
+              <h3>What is Dungeon World?</h3>
+            </Grid.Column>
+          </Grid>
 
-        {/* About Our App */}
-        <h2 class="ui center aligned grid">About our app</h2>
-        <p class="ui center aligned grid">
-          Focus less on managing, and more on playing
-        </p>
-        <Grid.Row columns={2}>
-          {/* Benefits of digital app */}
-          <Grid.Column>
-            <Icon name="hourglass outline" />
-            <h3>No more paper</h3>
-            <p>
-              No need to print anything out, you can keep track of everything
-              you need to through the app.
-            </p>
-          </Grid.Column>
-          <Grid.Column>
-            {/* <Icon name="paint brush" />
-            <Icon name="lightbulb outline" />
-            <Icon name="map outline" /> */}
-            <Icon name="sliders horizontal" />
-            <h3>Embrace customization</h3>
-            <p>
-              Dungeon World allows for endless customization. Use DW manager to
-              create materials based on your customizations. Just enter your
-              custom details and the app can generate a printable pdf of your
-              custom class.
-            </p>
-          </Grid.Column>
-          {/* Manage Sessions */}
-          <Grid.Column>
-            {/* <Icon name="group" /> */}
-            <Icon name="calendar check outline" />
-            <h3>Manage sessions</h3>
-            <p>Streamline communication between the GM and the players.</p>
-            <p></p>
-          </Grid.Column>
-          {/* Create, share, expand everything */}
-          <Grid.Column>
-            <Icon name="group" />
-            <h3>Create, share, expand everything</h3>
-            <p>
-              Share your creations through the app, and browse what others have
-              created to expand your own play sessions.
-            </p>
-          </Grid.Column>
-        </Grid.Row>
+          <p>Dungeon World is an open source table top roleplaying game.</p>
+        </Grid.Column>
+        {/* Exp from building the story */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={2}>
+              <Icon name="conversation" size="large" />
+              {/* <Icon name="beer" size="large" /> */}
+            </Grid.Column>
+            <Grid.Column width="fluid">
+              <h3>Gain experience from building the story</h3>
+            </Grid.Column>
+          </Grid>
 
-        {/* CTA */}
-        <h2 class="ui center aligned grid">Ready to begin your adventure?</h2>
-        <Grid.Row columns={3}>
-          {/* - download the app */}
-          <Grid.Column>
-            <Icon name="download" />
-            <h3>Download the app</h3>
-            <p>DW Manager is a PWA [Instructions for downloading]</p>
-          </Grid.Column>
-          {/* - create an account */}
-          <Grid.Column>
-            <Icon name="vcard" />
-            {/* <Icon name="user" /> */}
-            {/* <Icon name="id card" /> */}
-            <h3>Create your account</h3>
-            <p>
-              Create an account to play sessions with others [Benefits of
-              creating an account]
-            </p>
-          </Grid.Column>
-          {/* - learn more about DW  */}
-          <Grid.Column>
-            <Icon name="question circle" />
-            <h3>Still have questions?</h3>
-            <p>
-              Learn more about Dungeon World [link to more in-depth
-              rules/description of mechanics, etc.]
-            </p>
-            <Link
-              to="/pdfviewer"
-              params={{
-                pdfSrc:
-                  "https://drive.google.com/file/d/0B8_Fz4m5hcoiTXpTbklDOF9iUHc/preview?resourcekey=0-xI_68aH1lllySOdEovKvPQ",
-              }}
-              pdfSrc={{ pdfSrc }}
-            >
-              Learn more
-            </Link>
-          </Grid.Column>
-        </Grid.Row>
+          <p>
+            Dungeon World's simple rules happen based on what's happening within
+            the game, so you spend more time talking about the action and less
+            time talking about the rules.
+          </p>
+        </Grid.Column>
+        {/* Vs DnD */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="trophy" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>How's it different from DnD?</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>Dungeon World uses the Powered by the Apocalypse engine.</p>
+          <p>
+            Story-driven world building and character development - experience
+            is gained by progressing the story and developing your character.
+          </p>
+        </Grid.Column>
+      </Grid>
+
+      {/* About Our App */}
+      <Grid columns={1}>
+        <Grid.Column textAlign="center">
+          <h2 class="ui center aligned header">About our app</h2>
+          <p class="ui center aligned grid">
+            Focus less on managing, and more on playing
+          </p>
+        </Grid.Column>
+      </Grid>
+
+      <Grid columns={2}>
+        {/* Benefits of digital app */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="hourglass outline" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>No more paper</h3>
+            </Grid.Column>
+          </Grid>
+          <p>
+            No need to print anything out, you can keep track of everything you
+            need to through the app.
+          </p>
+        </Grid.Column>
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              {/* <Icon name="paint brush" size="large" />
+            <Icon name="lightbulb outline" size="large" />
+            <Icon name="map outline" size="large" /> */}
+              <Icon name="sliders horizontal" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Embrace customization</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>
+            Dungeon World allows for endless customization. Use DW manager to
+            create materials based on your customizations. Just enter your
+            custom details and the app can generate a printable pdf of your
+            custom class.
+          </p>
+        </Grid.Column>
+        {/* Manage Sessions */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              {/* <Icon name="group" size="large" /> */}
+              <Icon name="calendar check outline" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Manage sessions</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>Streamline communication between the GM and the players.</p>
+          <p></p>
+        </Grid.Column>
+        {/* Create, share, expand everything */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="group" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Create, share, expand everything</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>
+            Share your creations through the app, and browse what others have
+            created to expand your own play sessions.
+          </p>
+        </Grid.Column>
+      </Grid>
+
+      {/* CTA */}
+      <h2 class="ui center aligned header">Ready to begin your adventure?</h2>
+      <Grid columns={3}>
+        {/* - download the app */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="download" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Download the app</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>DW Manager is a PWA [Instructions for downloading]</p>
+        </Grid.Column>
+        {/* - create an account */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="vcard" size="large" />
+              {/* <Icon name="user" size="large" /> */}
+              {/* <Icon name="id card" size="large" /> */}
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Create your account</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>
+            Create an account to play sessions with others [Benefits of creating
+            an account]
+          </p>
+        </Grid.Column>
+        {/* - learn more about DW  */}
+        <Grid.Column>
+          <Grid centered padded columns={2}>
+            <Grid.Column width={1}>
+              <Icon name="question circle" size="large" />
+            </Grid.Column>
+            <Grid.Column>
+              <h3>Still have questions?</h3>
+            </Grid.Column>
+          </Grid>
+
+          <p>
+            Learn more about Dungeon World [link to more in-depth
+            rules/description of mechanics, etc.]
+          </p>
+          <Link to="/pdfviewer" state={{ pdfSrc }}>
+            Learn more
+          </Link>
+        </Grid.Column>
       </Grid>
     </React.Fragment>
   );
--- src/app/PdfViewer/index.js
+++ src/app/PdfViewer/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 // import { Link, useHistory } from "gatsby";
-import { useParams } from "gatsby";
 
 const PdfViewer = (props) => {
-	// const { params } = useParams();
-	return (
-		<React.Fragment>
-			{/* {{ pdf in iframe }} */}
-			{props.pdfSrc}
-			<iframe
-				style={{ "min-height": "100vh" }}
-				src={props.pdfSrc}
-				width="100%"
-				height="auto"
-			/>
-		</React.Fragment>
-	);
+  // const { params } = useParams();
+  return (
+    <React.Fragment>
+      {/* {{ pdf in iframe }} */}
+      <iframe
+        style={{ "min-height": "100vh" }}
+        src={props.location.state.pdfSrc}
+        width="100%"
+        height="auto"
+      />
+    </React.Fragment>
+  );
 };
 
 export default PdfViewer;
