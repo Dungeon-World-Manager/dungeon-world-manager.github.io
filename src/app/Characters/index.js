a8e361f5fe03a2407d6b2bf4420dd7aeba31ba76
Ty Streets <tstreets@iu.edu>
2022-03-22 17:50:30 -0400

    Fixed warnings

--- src/app/Characters/index.js
+++ src/app/Characters/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'semantic-ui-react';
 import { getCharacterInfo } from '../../functions/db';
 import CustomCard from '../components/CustomCard';
@@ -13,7 +13,7 @@ const CharactersList = () => {
 		loadCharacterData();
 	});
 	async function loadCharacterData() {
-		if (dataLoaded == true) {
+		if (dataLoaded === true) {
 			return;
 		} else {
 			var characters = await getCharacterInfo();
