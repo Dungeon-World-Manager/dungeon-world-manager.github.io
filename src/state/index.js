9681d60b713f71a114843f92cc4f243c3af7e25b
Ty Streets <tstreets@iu.edu>
2022-03-22 17:48:36 -0400

    Updated char list to use cards

--- src/state/index.js
+++ src/state/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import testState from './testState';
 import authState from './authState';
 import classesState from './classesState';
+import charactersState from './charactersState';
 
 const State = React.createContext();
 
 export const Provider = ({ children }) => {
-    const [state, setState] = React.useState({});
+	const [state, setState] = React.useState({});
 
-    const stateValues = {
-        test: testState(state, setState),
-        auth: authState(state, setState),
-        classes: classesState(state, setState),
-    };
-    return <State.Provider value={stateValues}>{children}</State.Provider>;
+	const stateValues = {
+		test: testState(state, setState),
+		auth: authState(state, setState),
+		classes: classesState(state, setState),
+		characters: charactersState(state, setState),
+	};
+	return <State.Provider value={stateValues}>{children}</State.Provider>;
 };
 
 export default State;
