1a283945d86bbb32cfd505125c0863fd62d99b1a
Ty Streets <tstreets@iu.edu>
2022-04-11 18:16:51 -0400

    Moved card group from grid

--- src/app/Sessions/index.js
+++ src/app/Sessions/index.js
@@ -1,138 +1,152 @@
-import React, { useState } from "react";
-import { Grid, Header, Card, Icon, Button } from "semantic-ui-react";
-import "semantic-ui-css/semantic.css";
+import React, { useState } from 'react';
+import { Grid, Header, Card, Icon, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.css';
 // import New from './new';
 // import Edit from './edit';
-import State from "../../state";
-import { getSessions } from "../../functions/db";
-import { Link } from "gatsby";
+import State from '../../state';
+import { getSessions } from '../../functions/db';
+import { Link } from 'gatsby';
 
 const Session = () => {
-  const [loadedData, setLoadedData] = React.useState(false);
-  const state = React.useContext(State);
-  const stateSessions = state.sessions;
-  const stateAuth = state.auth;
+	const [loadedData, setLoadedData] = React.useState(false);
+	const state = React.useContext(State);
+	const stateSessions = state.sessions;
+	const stateAuth = state.auth;
 
-  React.useEffect(() => {
-    attemptLoadSessions();
-  });
+	React.useEffect(() => {
+		attemptLoadSessions();
+	});
 
-  async function attemptLoadSessions() {
-    if (loadedData) return;
-    setLoadedData(true);
-    try {
-      const sessions = await getSessions();
-      stateSessions.loadSessionsList(sessions);
-    } catch {
-      console.log("Error loading sessions");
-    }
-  }
-  console.log(stateAuth.user.id);
+	async function attemptLoadSessions() {
+		if (loadedData) return;
+		setLoadedData(true);
+		try {
+			const sessions = await getSessions();
+			stateSessions.loadSessionsList(sessions);
+		} catch {
+			console.log('Error loading sessions');
+		}
+	}
+	console.log(stateAuth.user.id);
 
-  // Opening and closing form functions
-  const [newSessionOpen, setNewSessionOpen] = React.useState(false);
+	// Opening and closing form functions
+	const [newSessionOpen, setNewSessionOpen] = React.useState(false);
 
-  function openNewSession() {
-    setNewSessionOpen(true);
-  }
+	function openNewSession() {
+		setNewSessionOpen(true);
+	}
 
-  function closeNewSession() {
-    setNewSessionOpen(false);
-  }
+	function closeNewSession() {
+		setNewSessionOpen(false);
+	}
 
-  const [editSessionOpen, setEditSessionOpen] = React.useState(false);
+	const [editSessionOpen, setEditSessionOpen] = React.useState(false);
 
-  function openEditSession() {
-    setEditSessionOpen(true);
-  }
+	function openEditSession() {
+		setEditSessionOpen(true);
+	}
 
-  function closeEditSession() {
-    setEditSessionOpen(false);
-  }
+	function closeEditSession() {
+		setEditSessionOpen(false);
+	}
 
-  //Retrieving data functions
-  //   const retrieveAll = React.useRef(false);
+	//Retrieving data functions
+	//   const retrieveAll = React.useRef(false);
 
-  //   React.useEffect(() => {
-  //     attemptLoadSessions();
-  //   });
+	//   React.useEffect(() => {
+	//     attemptLoadSessions();
+	//   });
 
-  //   async function attemptLoadSessions() {
-  //     if (retrieveAll.current) return;
-  //     retrieveAll.current = true;
-  //     try {
-  //         const sessions = await getSessions();
-  //         stateSessions.loadPublicSession(sessions);
-  //     } catch {}
-  // }
+	//   async function attemptLoadSessions() {
+	//     if (retrieveAll.current) return;
+	//     retrieveAll.current = true;
+	//     try {
+	//         const sessions = await getSessions();
+	//         stateSessions.loadPublicSession(sessions);
+	//     } catch {}
+	// }
 
-  return (
-    <React.Fragment>
-      <Grid centered columns="2">
-        <Grid.Column>
-          <Header as="h1">Sessions List</Header>
-        </Grid.Column>
-        <Grid.Column textAlign="right" width="4">
-          <Icon color="green" name="plus" size="big" onClick={openNewSession} />
-        </Grid.Column>
+	return (
+		<React.Fragment>
+			<Grid centered columns='2'>
+				<Grid.Column>
+					<Header as='h1'>Sessions List</Header>
+				</Grid.Column>
+				<Grid.Column textAlign='right' width='4'>
+					<Icon
+						color='green'
+						name='plus'
+						size='big'
+						onClick={openNewSession}
+					/>
+				</Grid.Column>
 
-        {/* {newSessionOpen ? (
+				{/* {newSessionOpen ? (
 					<New closeNewSession={closeNewSession} />
 				) : null} */}
-        <Grid.Row stackable>
-          <Card.Group itemsPerRow="2">
-            {/* Loop through sessions and their info cards */}
-            {stateSessions.list.flatMap((curSession) => {
-              console.log(curSession);
-              return (
-                <Card>
-                  <Card.Content>
-                    <Card.Header>{curSession.description}</Card.Header>
-                    <Card.Meta>{curSession.creatorName}</Card.Meta>
-                    <Card.Description>{curSession.meetingDay}</Card.Description>
-                    <Card.Description>
-                      {curSession.meetingTime}
-                    </Card.Description>
-                    <Card.Description>
-                      {curSession.sessionDuration}
-                    </Card.Description>
-                    <Card.Meta>
-                      {curSession.numbPlayers + " players" ?? null}
-                    </Card.Meta>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <Button.Group fluid>
-                      {/* display edit info if logged in */}
-                      {stateAuth.user.id ? (
-                        <React.Fragment>
-                          <Button color="red">Trash</Button>
-                          <Button.Or />
-                          <Button color="orange" onClick={openEditSession}>
-                            Edit
-                          </Button>
-                        </React.Fragment>
-                      ) : (
-                        <Button
-                          color="blue"
-                          as={Link}
-                          to={`/sessions/view#${curSession.id}`}
-                        >
-                          Join
-                        </Button>
-                      )}
-                    </Button.Group>
-                  </Card.Content>
-                </Card>
-              );
-            })}
-          </Card.Group>
-        </Grid.Row>
-      </Grid>
-      {/* {editSessionOpen ? (
+				<Grid.Row stackable>
+					<Grid.Column></Grid.Column>
+				</Grid.Row>
+			</Grid>
+			<Card.Group itemsPerRow='2'>
+				{/* Loop through sessions and their info cards */}
+				{stateSessions.list.flatMap(curSession => {
+					console.log(curSession);
+					return (
+						<Card>
+							<Card.Content>
+								<Card.Header>
+									{curSession.description}
+								</Card.Header>
+								<Card.Meta>{curSession.creatorName}</Card.Meta>
+								<Card.Description>
+									{curSession.meetingDay}
+								</Card.Description>
+								<Card.Description>
+									{curSession.meetingTime}
+								</Card.Description>
+								<Card.Description>
+									{curSession.sessionDuration}
+								</Card.Description>
+								<Card.Meta>
+									{curSession.numbPlayers + ' players' ??
+										null}
+								</Card.Meta>
+							</Card.Content>
+							<Card.Content extra>
+								<Button.Group fluid>
+									{/* display edit info if logged in */}
+									{stateAuth.user.id ? (
+										<React.Fragment>
+											<Button color='red'>Trash</Button>
+											<Button.Or />
+											<Button
+												color='orange'
+												onClick={openEditSession}
+											>
+												Edit
+											</Button>
+										</React.Fragment>
+									) : (
+										<Button
+											color='blue'
+											as={Link}
+											to={`/sessions/view#${curSession.id}`}
+										>
+											Join
+										</Button>
+									)}
+								</Button.Group>
+							</Card.Content>
+						</Card>
+					);
+				})}
+			</Card.Group>
+			{/* {editSessionOpen ? (
 				<Edit closeEditSession={closeEditSession} />
 			) : null} */}
-    </React.Fragment>
-  );
+		</React.Fragment>
+	);
 };
 
 export default Session;
