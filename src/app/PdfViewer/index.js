41bbf0035be024afca7bf5ff56291a0e228186ec
Ty Streets <tstreets@iu.edu>
2022-04-18 19:29:34 -0400

    Cleaned up errors for pdfViewer

--- src/app/PdfViewer/index.js
+++ src/app/PdfViewer/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 // import { Link, useHistory } from "gatsby";
 
 const PdfViewer = props => {
-    // const { params } = useParams();
-    return (
-        <React.Fragment>
-            {/* {{ pdf in iframe }} */}
-            <iframe
-                style={{ 'min-height': '100vh' }}
-                src={props.location.state.pdfSrc}
-                width='100%'
-                height='auto'
-            />
-        </React.Fragment>
-    );
+	// const { params } = useParams();
+	return (
+		<React.Fragment>
+			{/* {{ pdf in iframe }} */}
+			<iframe
+				style={{ minHeight: '100vh' }}
+				src={props.location.state.pdfSrc || ''}
+				width='100%'
+				height='auto'
+			/>
+		</React.Fragment>
+	);
 };
 
 export default PdfViewer;
